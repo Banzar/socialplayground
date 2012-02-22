c340d4676aa12ce0abdaab51b049038f3f01376a
David Ledford <dcledford@ymail.com>
2012-02-21 19:14:15 -0500

    photo added to list items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ActiveRecord::Base
 	belongs_to :user
-	validates_presence_of :item, :description
+	validates_presence_of :item, :description, :state
+
+	attr_accessor :item, :description, :state, :photo_file_name
+
+	has_attached_file :photo,
+	:styles => { :large => "400x" }, 
+	:default_url => 'images/shirt.png',
+  :storage => :s3,
+  :bucket => ENV['S3_BUCKET2'],
+  :s3_credentials => {
+    :access_key_id => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']
+  }
 end
