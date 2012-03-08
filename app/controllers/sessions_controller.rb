b587541924a9609bc035f00e498bb3a8388ccccd
David Ledford <dcledford@ymail.com>
2012-03-07 23:23:00 -0500

    removed the accept confirmation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
+			params[:zip] = { :zip_code => '2' }
       session[:user_id] = user.id
       redirect_to root_url
+			user.update_attributes(params[:zip])
     else
       flash[:notice] = "Invalid email or password"
       render "new"
